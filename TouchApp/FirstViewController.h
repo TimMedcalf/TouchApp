f37f197542d02f2714e04780f7a3688a30948ab7
Tim Medcalf <tim@kadixi.com>
2011-08-05 16:53:20 +0100

    Initial Commit

---
+++ TouchApp/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  TouchApp
+//
+//  Created by Tim Medcalf on 05/08/2011.
+//  Copyright 2011 ErgoThis Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ TouchApp/TouchAppAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TouchAppAppDelegate.h
+//  TouchApp
+//
+//  Created by Tim Medcalf on 05/08/2011.
+//  Copyright 2011 ErgoThis Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TouchAppAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
