d2bc3eab7b2db4b1369973a523ddfcd8b3871abe
Tim Medcalf <tim@kadixi.com>
2011-08-06 15:00:28 +0100

    News table cells configured

--- TouchApp/NewsViewController.m
+++ TouchApp/NewsViewController.m
@@ -8,6 +8,9 @@
 
 #import "NewsViewController.h"
 
+static NSInteger CellTitleTag = 50;
+static NSInteger CellSubTitleTag = 51;
+
 @implementation NewsViewController
 
 - (id)initWithStyle:(UITableViewStyle)style
@@ -91,7 +94,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
   // Return the number of rows in the section.
-  return (section == 0) ? 1 : 0;
+  return (section == 0) ? 1 : 1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -119,8 +122,69 @@
       cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
       if (cell == nil) {
         cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+        
+        //we can't se the frame of the default labels and disclosure indicator
+        //so lets ignore them and just add some of our own to the view.
+        //if we tag them we can retrieve them later in the method so that we can
+        //set the properties that change (i.e. the text)
+        UILabel *titleLabel = [[UILabel alloc] init];
+        titleLabel.tag = CellTitleTag;
+        UILabel *subtitleLabel = [[UILabel alloc] init];
+        subtitleLabel.tag = CellSubTitleTag;
+        UIImageView *disclosure = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"go"]];
+        //no need to tag the disclosure indicator cos we don't need to do anything with it once its added to the view
+        // Set the size, font, foreground color, background color
+        titleLabel.textColor = [UIColor blackColor]; 
+        titleLabel.textAlignment = UITextAlignmentLeft; 
+        titleLabel.contentMode = UIViewContentModeCenter; 
+        titleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
+        titleLabel.numberOfLines = 0; 
+        
+        
+        subtitleLabel.textColor = [UIColor grayColor]; 
+        subtitleLabel.textAlignment = UITextAlignmentLeft; 
+        subtitleLabel.contentMode = UIViewContentModeCenter; 
+        subtitleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
+        subtitleLabel.numberOfLines = 0;
+        
+        
+        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+          //iPad
+          titleLabel.frame = CGRectMake(50,20,535,25);
+          titleLabel.font = [UIFont fontWithName:@"Helvetica" size:21]; 
+          
+          subtitleLabel.frame = CGRectMake(50,45,535,22);
+          subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:15];           
+          
+          disclosure.frame = CGRectMake(673, 19, 45, 45);
+        }
+        else {
+          //iPhone
+          titleLabel.frame = CGRectMake(17,16,247,15);
+          titleLabel.font = [UIFont fontWithName:@"Helvetica" size:14]; 
+          
+          subtitleLabel.frame = CGRectMake(17,31,247,15);
+          subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:10]; 
+
+          disclosure.frame = CGRectMake(273, 14, 30, 30);
+        }
+        //now they're all set up, add them to the cell's view and release them
+        [cell addSubview:titleLabel];
+        [cell addSubview:subtitleLabel];
+        [cell addSubview:disclosure];
+        [titleLabel release];
+        [subtitleLabel release];
+        [disclosure release];
       }
-      // Configure the cell...
+      // so, now to configure the cell...
+      // first grab hold of the cell elements we need
+      UILabel *titleLabel = (UILabel *)[cell viewWithTag:CellTitleTag];
+      UILabel *subtitleLabel = (UILabel *)[cell viewWithTag:CellSubTitleTag];
+      
+      //got them...now set the text we want...
+      titleLabel.text = @"Tone 44V | Fennesz \"Seven Stars\"";
+      subtitleLabel.text = @"Posted on July 18, 2011";
+
     }
   }
   return cell;
@@ -130,11 +194,17 @@
 {
   if (indexPath.section == 0)
   {
-    return 128 + 1;
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+      return 307 + 1;
+    else
+      return 128 + 1;
   }
   else
   {
-    return 44;
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+      return 87;
+    else
+      return 57;
   }  
 }
 
