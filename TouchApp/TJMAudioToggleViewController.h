9c3d58b51fe5afb98d48eed5c0d8e28a4c259188
Tim Medcalf <tim@kadixi.com>
2011-09-07 19:07:51 +0100

    Added new view controller subclass for audio toggling on non tableview controllers

---
+++ TouchApp/TJMAudioToggleViewController.h
@@ -0,0 +1,13 @@
+//
+//  TJMAudioToggleViewController.h
+//  TouchApp
+//
+//  Created by Tim Medcalf on 07/09/2011.
+//  Copyright 2011 ErgoThis Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJMAudioToggleViewController : UIViewController
+
+@end
