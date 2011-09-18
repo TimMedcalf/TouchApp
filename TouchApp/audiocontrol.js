a2f29b345f8dba5556a687536c3b45b3e361156b
Dave Knapik <daveknapik@gmail.com>
2011-09-18 00:32:07 -0400

    Changed 'click' to 'tap' in JS toggle

--- TouchApp/audiocontrol.js
+++ TouchApp/audiocontrol.js
@@ -1,11 +1,11 @@
 $(function(){
   
   var pause = function(){
-    $('#playerwrapper').html("<div><strong>Pause</strong><br /><span class='subtitle'>Click here to pause the audio stream</span></div>");
+    $('#playerwrapper').html("<div><strong>Pause</strong><br /><span class='subtitle'>Tap here to pause the audio stream</span></div>");
   }
   
   var play = function(){
-    $('#playerwrapper').html("<div><strong>Play</strong><br /><span class='subtitle'>Click here to stream audio</span></div>");
+    $('#playerwrapper').html("<div><strong>Play</strong><br /><span class='subtitle'>Tap here to stream audio</span></div>");
   }
   
   $('#playerwrapper').toggle(pause,play);
