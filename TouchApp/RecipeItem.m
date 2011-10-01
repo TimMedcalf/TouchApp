13f3514f7422a695bc5bdd3236f194c153539d03
Dave Knapik <daveknapik@gmail.com>
2011-10-01 15:25:59 -0400

    Lowercased 'by' in Recipes byline

--- TouchApp/RecipeItem.m
+++ TouchApp/RecipeItem.m
@@ -72,7 +72,7 @@ NSString *const Key_Recipe_PubDate = @"pubDate";
   return [NSString stringWithFormat:@"<html><head><meta name=\"viewport\" content=\"width=device-width\" />"
           "<link rel=\"stylesheet\" media=\"only screen and (max-device-width: 480px)\" href=\"mobile.css\" />"
           "<link rel=\"stylesheet\" media=\"only screen and (min-device-width: 481px) and (max-device-width: 1024px)\" href=\"ipad.css\" /></head>"
-          "<body><div id='headerwrapper'><div id='headercell'><div id='title'><strong>%@</strong><br /><span id='byline'>By %@</span></div></div></div>"
+          "<body><div id='headerwrapper'><div id='headercell'><div id='title'><strong>%@</strong><br /><span id='byline'>by %@</span></div></div></div>"
           "<div id=\"bodycopycontainer\"><p class='bodycopy'>%@</p></div></body></html>", self.recipeExcerpt,self.recipeTitle,self.recipeDescription];
 }
 
