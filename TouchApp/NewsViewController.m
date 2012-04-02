86629507a00318c3b48cee506d2f1e093bb764c8
Tim Medcalf <tim@kadixi.com>
2012-04-02 21:53:33 +0100

    Major clean up of NewsVC to move to tableHeaderView for the Banner image instead of multiple sections..

--- TouchApp/NewsViewController.m
+++ TouchApp/NewsViewController.m
@@ -49,8 +49,13 @@ static NSInteger CellSubTitleTag = 51;
   [super viewDidLoad];
   [FlurryAnalytics logAllPageViews:self.navigationController];
   
-  
   self.navigationItem.title= @"";
+  
+  UIImage *header = [[UIImage alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"news_header" ofType:@"png"]];
+  UIImageView *headerView = [[UIImageView alloc]initWithImage:header];
+  [header release];
+  self.tableView.tableHeaderView = headerView;
+  [headerView release];
   
   UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"News" style:UIBarButtonItemStyleBordered target:nil action:nil];
   self.navigationItem.backBarButtonItem = backButton;
@@ -118,128 +123,95 @@ static NSInteger CellSubTitleTag = 51;
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
   // Return the number of sections.
-  return 2;
+  return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
   // Return the number of rows in the section.
-  return (section == 0) ? 1 : [self.newsList.items count];
+  return [self.newsList.items count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
   NSString *CellIdentifier;
   UITableViewCell *cell;
-  switch (indexPath.section)
-  {
-    case 0:
+  CellIdentifier = @"NewsItem";
+  cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+    cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    //we can't se the frame of the default labels and disclosure indicator
+    //so lets ignore them and just add some of our own to the view.
+    //if we tag them we can retrieve them later in the method so that we can
+    //set the properties that change (i.e. the text)
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.tag = CellTitleTag;
+    UILabel *subtitleLabel = [[UILabel alloc] init];
+    subtitleLabel.tag = CellSubTitleTag;
+    UIImageView *disclosure = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"go"]];
+    //no need to tag the disclosure indicator cos we don't need to do anything with it once its added to the view
+    // Set the size, font, foreground color, background color
+    titleLabel.textColor = [UIColor blackColor]; 
+    titleLabel.textAlignment = UITextAlignmentLeft; 
+    titleLabel.contentMode = UIViewContentModeCenter; 
+    titleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
+    titleLabel.numberOfLines = 0; 
+    
+    
+    subtitleLabel.textColor = [UIColor grayColor]; 
+    subtitleLabel.textAlignment = UITextAlignmentLeft; 
+    subtitleLabel.contentMode = UIViewContentModeCenter; 
+    subtitleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
+    subtitleLabel.numberOfLines = 0;
+    
+    
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
     {
-      CellIdentifier = @"NewsHeader";
-      cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-      if (cell == nil) {
-          cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-          cell.selectionStyle = UITableViewCellSelectionStyleNone;
-      }
-      // Configure the cell...
-      UIImage *header = [[UIImage alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"news_header" ofType:@"png"]];
-      [cell.imageView setImage:header];
-      [header release];
-      break;
+      //iPad
+      titleLabel.frame = CGRectMake(50,20,535,25);
+      titleLabel.font = [UIFont fontWithName:@"Helvetica" size:21]; 
+      
+      subtitleLabel.frame = CGRectMake(50,45,535,22);
+      subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:15];           
+      
+      disclosure.frame = CGRectMake(673, 19, 45, 45);
     }
-    case 1:
-    default:
+    else
     {
-      CellIdentifier = @"NewsItem";
-      cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-      if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-        
-        //we can't se the frame of the default labels and disclosure indicator
-        //so lets ignore them and just add some of our own to the view.
-        //if we tag them we can retrieve them later in the method so that we can
-        //set the properties that change (i.e. the text)
-        UILabel *titleLabel = [[UILabel alloc] init];
-        titleLabel.tag = CellTitleTag;
-        UILabel *subtitleLabel = [[UILabel alloc] init];
-        subtitleLabel.tag = CellSubTitleTag;
-        UIImageView *disclosure = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"go"]];
-        //no need to tag the disclosure indicator cos we don't need to do anything with it once its added to the view
-        // Set the size, font, foreground color, background color
-        titleLabel.textColor = [UIColor blackColor]; 
-        titleLabel.textAlignment = UITextAlignmentLeft; 
-        titleLabel.contentMode = UIViewContentModeCenter; 
-        titleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
-        titleLabel.numberOfLines = 0; 
-        
-        
-        subtitleLabel.textColor = [UIColor grayColor]; 
-        subtitleLabel.textAlignment = UITextAlignmentLeft; 
-        subtitleLabel.contentMode = UIViewContentModeCenter; 
-        subtitleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
-        subtitleLabel.numberOfLines = 0;
-        
-        
-        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-          //iPad
-          titleLabel.frame = CGRectMake(50,20,535,25);
-          titleLabel.font = [UIFont fontWithName:@"Helvetica" size:21]; 
-          
-          subtitleLabel.frame = CGRectMake(50,45,535,22);
-          subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:15];           
-          
-          disclosure.frame = CGRectMake(673, 19, 45, 45);
-        }
-        else {
-          //iPhone
-          titleLabel.frame = CGRectMake(17,16,247,15);
-          titleLabel.font = [UIFont fontWithName:@"Helvetica" size:14]; 
-          
-          subtitleLabel.frame = CGRectMake(17,31,247,15);
-          subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:10]; 
-
-          disclosure.frame = CGRectMake(273, 14, 30, 30);
-        }
-        //now they're all set up, add them to the cell's view and release them
-        [cell addSubview:titleLabel];
-        [cell addSubview:subtitleLabel];
-        [cell addSubview:disclosure];
-        [titleLabel release];
-        [subtitleLabel release];
-        [disclosure release];
-      }
-      // so, now to configure the cell...
-      // first grab hold of the cell elements we need
-      NewsItem *currentItem = [self.newsList.items objectAtIndex:indexPath.row];
+      //iPhone
+      titleLabel.frame = CGRectMake(17,16,247,15);
+      titleLabel.font = [UIFont fontWithName:@"Helvetica" size:14]; 
       
-      UILabel *titleLabel = (UILabel *)[cell viewWithTag:CellTitleTag];
-      UILabel *subtitleLabel = (UILabel *)[cell viewWithTag:CellSubTitleTag];
+      subtitleLabel.frame = CGRectMake(17,31,247,15);
+      subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:10]; 
       
-      //got them...now set the text we want...
-      titleLabel.text = currentItem.title;
-      subtitleLabel.text = currentItem.pubDate;//[NSDateFormatter localizedStringFromDate:currentItem.pubDate dateStyle:NSDateFormatterMediumStyle timeStyle:kCFDateFormatterShortStyle];
+      disclosure.frame = CGRectMake(273, 14, 30, 30);
     }
+    //now they're all set up, add them to the cell's view and release them
+    [cell addSubview:titleLabel];
+    [cell addSubview:subtitleLabel];
+    [cell addSubview:disclosure];
+    [titleLabel release];
+    [subtitleLabel release];
+    [disclosure release];
   }
-  cell.selectionStyle = UITableViewCellSelectionStyleNone;
+  // so, now to configure the cell...
+  // first grab hold of the cell elements we need
+  NewsItem *currentItem = [self.newsList.items objectAtIndex:indexPath.row];
+  
+  UILabel *titleLabel = (UILabel *)[cell viewWithTag:CellTitleTag];
+  UILabel *subtitleLabel = (UILabel *)[cell viewWithTag:CellSubTitleTag];
+  
+  //got them...now set the text we want...
+  titleLabel.text = currentItem.title;
+  subtitleLabel.text = currentItem.pubDate;//[NSDateFormatter localizedStringFromDate:currentItem.pubDate dateStyle:NSDateFormatterMediumStyle timeStyle:kCFDateFormatterShortStyle];
   return cell;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  if ((indexPath.section == 0) && (indexPath.row == 0))
-  {
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-      return 307 + 1;
-    else
-      return 128 + 1;
-  }
-  else
-  {
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-      return 87;
-    else
-      return 58;
-  }  
+  return (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? 87 : 58;
 }
 
 
@@ -247,8 +219,6 @@ static NSInteger CellSubTitleTag = 51;
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  if (indexPath.section == 0) return;
-  
   NewsItem *curItem = [self.newsList.items objectAtIndex:indexPath.row];
   HTMLItemViewController *controller = [[HTMLItemViewController alloc] initWithNibName:@"HTMLItemViewController" bundle:nil];
   controller.HTMLString = curItem.htmlForWebView;
