43730f191f6a99ce4436c1e004d5826151dd947a
Tim Medcalf <tim@kadixi.com>
2012-10-17 21:54:11 +0100

    Updated all tables to use proper headers...

--- TouchApp/RecipeBookViewController.m
+++ TouchApp/RecipeBookViewController.m
@@ -47,6 +47,11 @@ static NSInteger CellSubTitleTag = 51;
   
   self.navigationItem.title= @"";
   
+  UIImage *header = [[UIImage alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"recipes_header" ofType:@"png"]];
+  UIImageView *headerView = [[UIImageView alloc]initWithImage:header];
+  self.tableView.tableHeaderView = headerView;
+
+  
   UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Recipes" style:UIBarButtonItemStyleBordered target:nil action:nil];
   self.navigationItem.backBarButtonItem = backButton;
   
@@ -119,133 +124,104 @@ static NSInteger CellSubTitleTag = 51;
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
   // Return the number of sections.
-  return 2;
+  return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
   // Return the number of rows in the section.
-  return (section == 0) ? 1 : [self.recipeList.items count];
+  return [self.recipeList.items count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
   NSString *CellIdentifier;
   UITableViewCell *cell;
-  switch (indexPath.section)
-  {
-    case 0:
+  CellIdentifier = @"RecipeBookItem";
+  cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    
+    //we can't se the frame of the default labels and disclosure indicator
+    //so lets ignore them and just add some of our own to the view.
+    //if we tag them we can retrieve them later in the method so that we can
+    //set the properties that change (i.e. the text)
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.tag = CellTitleTag;
+    UILabel *subtitleLabel = [[UILabel alloc] init];
+    subtitleLabel.tag = CellSubTitleTag;
+    UIImageView *disclosure = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"go"]];
+    //no need to tag the disclosure indicator cos we don't need to do anything with it once its added to the view
+    // Set the size, font, foreground color, background color
+    titleLabel.textColor = [UIColor blackColor]; 
+    titleLabel.textAlignment = UITextAlignmentLeft; 
+    titleLabel.contentMode = UIViewContentModeCenter; 
+    titleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
+    titleLabel.numberOfLines = 0; 
+    
+    
+    subtitleLabel.textColor = [UIColor grayColor]; 
+    subtitleLabel.textAlignment = UITextAlignmentLeft; 
+    subtitleLabel.contentMode = UIViewContentModeCenter; 
+    subtitleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
+    subtitleLabel.numberOfLines = 0;
+    
+    
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
     {
-      CellIdentifier = @"RecipeHeader";
-      cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-      if (cell == nil) {
-          cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-          cell.selectionStyle = UITableViewCellSelectionStyleNone;
-      }
-      // Configure the cell...
-      UIImage *header = [[UIImage alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"recipes_header" ofType:@"png"]];
-      [cell.imageView setImage:header];
-      break;
+      //iPad
+      titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+      titleLabel.frame = CGRectMake(50,20,cell.frame.size.width-195,25);
+      titleLabel.font = [UIFont fontWithName:@"Helvetica" size:21];
+      subtitleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+      subtitleLabel.frame = CGRectMake(50,45,cell.frame.size.width-195,22);
+      subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:15];
+      //disclosure.frame = CGRectMake(673, 19, 45, 45);
+      disclosure.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin;
+      disclosure.frame = CGRectMake(cell.frame.size.width-95, 19, 45, 45);
     }
-    case 1:
-    default:
+    else
     {
-      CellIdentifier = @"RecipeBookItem";
-      cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-      if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-        
-        //we can't se the frame of the default labels and disclosure indicator
-        //so lets ignore them and just add some of our own to the view.
-        //if we tag them we can retrieve them later in the method so that we can
-        //set the properties that change (i.e. the text)
-        UILabel *titleLabel = [[UILabel alloc] init];
-        titleLabel.tag = CellTitleTag;
-        UILabel *subtitleLabel = [[UILabel alloc] init];
-        subtitleLabel.tag = CellSubTitleTag;
-        UIImageView *disclosure = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"go"]];
-        //no need to tag the disclosure indicator cos we don't need to do anything with it once its added to the view
-        // Set the size, font, foreground color, background color
-        titleLabel.textColor = [UIColor blackColor]; 
-        titleLabel.textAlignment = UITextAlignmentLeft; 
-        titleLabel.contentMode = UIViewContentModeCenter; 
-        titleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
-        titleLabel.numberOfLines = 0; 
-        
-        
-        subtitleLabel.textColor = [UIColor grayColor]; 
-        subtitleLabel.textAlignment = UITextAlignmentLeft; 
-        subtitleLabel.contentMode = UIViewContentModeCenter; 
-        subtitleLabel.lineBreakMode = UILineBreakModeTailTruncation; 
-        subtitleLabel.numberOfLines = 0;
-        
-        
-        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-          //iPad
-          titleLabel.frame = CGRectMake(50,20,535,25);
-          titleLabel.font = [UIFont fontWithName:@"Helvetica" size:21]; 
-          
-          subtitleLabel.frame = CGRectMake(50,45,535,22);
-          subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:15];           
-          
-          disclosure.frame = CGRectMake(673, 19, 45, 45);
-        }
-        else {
-          //iPhone
-          titleLabel.frame = CGRectMake(17,16,247,15);
-          titleLabel.font = [UIFont fontWithName:@"Helvetica" size:14]; 
-          
-          subtitleLabel.frame = CGRectMake(17,31,247,15);
-          subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:10]; 
-
-          disclosure.frame = CGRectMake(273, 14, 30, 30);
-        }
-        //now they're all set up, add them to the cell's view and release them
-        [cell addSubview:titleLabel];
-        [cell addSubview:subtitleLabel];
-        [cell addSubview:disclosure];
-      }
-      // so, now to configure the cell...
-      // first grab hold of the cell elements we need
-      RecipeItem *currentItem = (self.recipeList.items)[indexPath.row];
-      
-      UILabel *titleLabel = (UILabel *)[cell viewWithTag:CellTitleTag];
-      UILabel *subtitleLabel = (UILabel *)[cell viewWithTag:CellSubTitleTag];
-      
-      //got them...now set the text we want...
-      titleLabel.text = currentItem.recipeTitle;
-      subtitleLabel.text = currentItem.recipeExcerpt;//[NSDateFormatter localizedStringFromDate:currentItem.pubDate dateStyle:NSDateFormatterMediumStyle timeStyle:kCFDateFormatterShortStyle];
+      //iPhone
+      titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+      titleLabel.frame = CGRectMake(17,16,cell.frame.size.width-81,15);
+      titleLabel.font = [UIFont fontWithName:@"Helvetica" size:14];
+      subtitleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+      subtitleLabel.frame = CGRectMake(17,31,cell.frame.size.width-81,15);
+      subtitleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:10];
+      disclosure.frame = CGRectMake(cell.frame.size.width-47, 14, 30, 30);
     }
+    //now they're all set up, add them to the cell's view and release them
+    [cell addSubview:titleLabel];
+    [cell addSubview:subtitleLabel];
+    [cell addSubview:disclosure];
   }
+  // so, now to configure the cell...
+  // first grab hold of the cell elements we need
+  RecipeItem *currentItem = (self.recipeList.items)[indexPath.row];
+  
+  UILabel *titleLabel = (UILabel *)[cell viewWithTag:CellTitleTag];
+  UILabel *subtitleLabel = (UILabel *)[cell viewWithTag:CellSubTitleTag];
+  
+  //got them...now set the text we want...
+  titleLabel.text = currentItem.recipeTitle;
+  subtitleLabel.text = currentItem.recipeExcerpt;//[NSDateFormatter localizedStringFromDate:currentItem.pubDate dateStyle:NSDateFormatterMediumStyle timeStyle:kCFDateFormatterShortStyle];
+
   cell.selectionStyle = UITableViewCellSelectionStyleNone;
   return cell;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  if ((indexPath.section == 0) && (indexPath.row == 0))
-  {
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-      return 307 + 1;
-    else
-      return 128 + 1;
-  }
+  if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+    return 87;
   else
-  {
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-      return 87;
-    else
-      return 58;
-  }  
+    return 58;
 }
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-  // Navigation logic may go here. Create and push another view controller.
-  //return immediately if user selected header image
-  if (indexPath.section == 0) return;
-  
+{  
   RecipeItem *curItem = (self.recipeList.items)[indexPath.row];
   RecipeItemViewController *controller = [[RecipeItemViewController alloc] initWithNibName:@"RecipeItemViewController" bundle:nil];
   controller.HTMLString = curItem.htmlForWebView;
