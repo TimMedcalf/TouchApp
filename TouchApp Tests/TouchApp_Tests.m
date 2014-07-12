62ad4a44b4fc0c34f759e8d16810964ed8788d4a
Tim Medcalf <tim@kadixi.com>
2014-07-12 08:16:03 +0100

    Added the unit testing target.

---
+++ TouchApp Tests/TouchApp_Tests.m
@@ -0,0 +1,34 @@
+//
+//  TouchApp_Tests.m
+//  TouchApp Tests
+//
+//  Created by Tim Medcalf on 11/07/2014.
+//  Copyright (c) 2014 ErgoThis Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TouchApp_Tests : XCTestCase
+
+@end
+
+@implementation TouchApp_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
