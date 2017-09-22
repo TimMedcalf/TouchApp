27c9822b4f5f2ac7cd81b2d045514d5461c3ed0c
Medcalf, Tim <tim.medcalf@sky.uk>
2017-09-22 10:37:29 +0100

    TM : Update to Xcode 9

--- Pods/Bugsee/Bugsee.framework/Versions/A/Headers/Bugsee.h
+++ Pods/Bugsee/Bugsee.framework/Versions/A/Headers/Bugsee.h
@@ -38,6 +38,13 @@ if (!condition) {[Bugsee logAssert:description withLocation:[NSString stringWith
  *  @return pass array of attachments here.
  */
 -(nonnull NSArray<BugseeAttachment* >*) bugseeAttachmentsForReport:(nonnull BugseeReport *)report;
+-(void) bugseeAttachmentsForReport:(nonnull BugseeReport *)report completionHandler:(nonnull BugseeAttachmentsDecisionBlock)decisionBlock;
+/**
+ *  Use this delegate to hanle new feedback messages in your app.
+ *
+ *  @param messages  text messages in array
+ */
+-(void) bugsee:(nonnull Bugsee *)bugsee didReceiveNewFeedback:(nonnull NSArray<NSString *> *)messages;
 
 @end
 
@@ -107,6 +114,11 @@ if (!condition) {[Bugsee logAssert:description withLocation:[NSString stringWith
 + (void) logEx:(nonnull NSDictionary*)entry;
 
 /**
+ *  Show feedback controller for contacting the user
+ */
++ (void) showFeedbackController;
+
+/**
  *  Use this method to filter network events and their properties.
  *
  *  Always call removeNetworkEventFilter method if you deallocate
@@ -115,6 +127,17 @@ if (!condition) {[Bugsee logAssert:description withLocation:[NSString stringWith
  *  @param filterBlock pass BugseeNetworkEvent into this block
  */
 + (void) setNetworkEventFilter:(nonnull BugseeNetworkEventFilterBlock)filterBlock;
+
+/**
+ *  Use this method to send info about your network event with bugsee report
+ *
+ *  @param event BugseeNetworkEvent 
+ *  @see BugseeNetworkEvent
+ */
++ (void) registerNetworkEvent:(nonnull BugseeNetworkEvent *)event;
+
++ (void) setDefaultFeedbackGreeting:(nonnull NSString *)greeting;
+
 /**
  *  Remove exists filter that was setup with setNetworkEventFilter: method
  */
@@ -128,7 +151,7 @@ if (!condition) {[Bugsee logAssert:description withLocation:[NSString stringWith
  *  @param email string with email
  *  @return YES on success, NO on falure
  */
-+ (BOOL) setEmail:(nonnull NSString *)email NS_SWIFT_NAME(setEmail(_:));;
++ (BOOL) setEmail:(nonnull NSString *)email NS_SWIFT_NAME(setEmail(_:));
 
 /**
  *  Get reporter's email
@@ -145,6 +168,38 @@ if (!condition) {[Bugsee logAssert:description withLocation:[NSString stringWith
 + (BOOL) clearEmail;
 
 /**
+ *  Set user attribute by ket
+ *
+ *  @param key string with unique key to set
+ *  @param value object to set (may be string, number of boolean)
+ *  @return YES on success, NO on falure
+ */
++ (BOOL) setAttribute:(NSString*_Nonnull)key withValue:(id _Nonnull )value NS_SWIFT_NAME(setAttribute(_:value:));
+
+/**
+ *  Get specific user attribute by key
+ *
+ *  @param key string with unique key to get
+ *  @return object or nil on failure
+ */
++ (id _Nullable ) getAttribute:(NSString*_Nonnull)key NS_SWIFT_NAME(getAttribute(_:));
+
+/**
+ *  Clear specific user attribute by key
+ *
+ *  @param key string with unique key to clear
+ *  @return NSString* with email on success, or nil on failure
+ */
++ (BOOL) clearAttribute:(NSString*_Nonnull)key NS_SWIFT_NAME(clearAttribute(_:));
+
+/**
+ *  Clear all user attributes
+ *
+ *  @return YES on success, NO on falure
+ */
++ (BOOL) clearAllAttributes NS_SWIFT_NAME(clearAllAttribute());
+
+/**
  *  Hides your view on video same thing you can get from Bugsee+UIView category
  *  view.bugseeProtectedView
  *
@@ -154,6 +209,32 @@ if (!condition) {[Bugsee logAssert:description withLocation:[NSString stringWith
 + (void) setView:(nonnull UIView *)view asHidden:(BOOL) isHidden NS_SWIFT_NAME(setView(_:asHidden:));
 + (BOOL) isViewHidden:(nonnull UIView *) view NS_SWIFT_NAME(isViewHidden(_:));
 
+
++ (void) setDefaultCrashPriority:(BugseeSeverityLevel) level;
++ (void) setDefaultBugPriority:(BugseeSeverityLevel) level;
+
+/**
+ *  Hides your keyboard, actualy we make it automaticaly for private fields.
+ *
+ *  @param isHidden YES to hide keyboard, by default is NO
+ */
++ (void) hideKeyboard:(BOOL) isHidden;
+
+/**
+ *  Crash simulators.
+ */
++ (void) testExceptionCrash;
++ (void) testSignalCrash;
+
+/**
+ *  Log managed excetpions.
+ *  @param name     
+ *  @param reason
+ *  @param frames   Arrays of strings, string for each frame
+ *  @param type     "xamarin", "unity", "cordova"
+ *  @param handled  bool value
+ */
++ (void) logException:(nonnull NSString *)name reason:(nonnull NSString*)reason frames:(nonnull NSArray*)frames type:(nonnull NSString*)type handled:(BOOL)handled NS_SWIFT_NAME(logException(name:reason:frames:type:handled:));
 
 @end
 
--- Pods/Bugsee/Bugsee.framework/Versions/A/Headers/BugseeConstants.h
+++ Pods/Bugsee/Bugsee.framework/Versions/A/Headers/BugseeConstants.h
@@ -7,13 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BugseeAttachment.h"
 
 #define BugseeTrue @(YES)
 #define BugseeFalse @(NO)
 
 @class BugseeNetworkEvent;
 typedef void (^BugseeNetworkFilterDecisionBlock)(BugseeNetworkEvent * _Nullable event );
-
+typedef void (^BugseeAttachmentsDecisionBlock)(NSArray<BugseeAttachment*>* _Nullable attachments);
 typedef void (^BugseeNetworkEventFilterBlock)(BugseeNetworkEvent * _Nonnull event, BugseeNetworkFilterDecisionBlock _Nonnull decisionBlock);
 
 typedef enum : NSUInteger {
@@ -23,6 +24,11 @@ typedef enum : NSUInteger {
     BugseeSeverityCritical = 4,
     BugseeSeverityBlocker = 5
 } BugseeSeverityLevel;
+
+typedef enum : NSUInteger {
+    BugseeFrameRateLow = 1,
+    BugseeFrameRateHigh = 2
+} BugseeFrameRate;
 
 /**
  *  BugseeStyleBasedOnStatusBarStyle setup style by current status bar style
@@ -38,13 +44,18 @@ extern NSString *const _Nonnull BugseeShakeToReportKey,
                 *const _Nonnull BugseeScreenshotToReportKey,
                 *const _Nonnull BugseeCrashReportKey,
                 *const _Nonnull BugseeKillDetectionKey,
+                *const _Nonnull BugseeFrameRateKey,
                 *const _Nonnull BugseeMaxFrameRateKey,
                 *const _Nonnull BugseeMinFrameRateKey,
                 *const _Nonnull BugseeMonitorNetworkKey,
+                *const _Nonnull BugseeMonitorWebSocketKey,
                 *const _Nonnull BugseeStatusBarInfoKey,
                 *const _Nonnull BugseeVideoEnabledKey,
                 *const _Nonnull BugseeStyleKey,
                 *const _Nonnull BugseeEnableMachExceptionsKey,
+                *const _Nonnull BugseeReportPrioritySelectorKey,
+                *const _Nonnull BugseeDefaultCrashPriorityKey,
+                *const _Nonnull BugseeDefaultBugPriorityKey,
                 *const _Nonnull BugseeCaptureLogsKey
 ;
 
@@ -75,6 +86,12 @@ extern NSString * const _Nonnull BugseeNetworkEventBegin;
 extern NSString * const _Nonnull BugseeNetworkEventComplete;
 extern NSString * const _Nonnull BugseeNetworkEventCancel;
 extern NSString * const _Nonnull BugseeNetworkEventError;
+
+extern NSString * const _Nonnull BugseeWebSocketEventOpen;
+extern NSString * const _Nonnull BugseeWebSocketEventSend;
+extern NSString * const _Nonnull BugseeWebSocketEventMessage;
+extern NSString * const _Nonnull BugseeWebSocketEventClose;
+extern NSString * const _Nonnull BugseeWebSocketEventError;
 
 extern NSString * const _Nonnull BugseeReportTypeBug;
 extern NSString * const _Nonnull BugseeReportTypeCrash;
